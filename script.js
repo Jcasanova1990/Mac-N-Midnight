1610e0eced9451b1bc4ea534fc61eec5e48353cf
Jcasanova1990 <jeremykcasanova@gmail.com>
2023-11-29 00:36:21 -0500

    'updated /spawn'

--- script.js
+++ script.js
@@ -37,37 +37,38 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function movePlayer(deltaX, deltaY) {
-      const player = document.getElementById("player");
-      const stepSize = 10;
-
-      const currentLeft = parseInt(player.style.left) || 0;
-
-      const currentTop = parseInt(player.style.top) || 0;
-      console.log(player.style.left, player.style.top);
-      const newLeft = currentLeft + deltaX;
-      const newTop = currentTop + deltaY;
-
-      // Set boundaries for left and right
-      const maxWidth = document.getElementById("game").offsetWidth;
-      const maxHeight = document.getElementById("game").offsetHeight;
-
-      // Ensure the new position is within bounds
-      const boundedLeft = Math.max(
-        0,
-        Math.min(newLeft, maxWidth - player.clientWidth),
-      );
-      const boundedTop = Math.max(
-        0,
-        Math.min(newTop, maxHeight - player.clientHeight),
-      );
-
-      player.style.left = `${boundedLeft}px`;
-      player.style.top = `${boundedTop}px`;
-
-      if (!attacking) {
-        player.style.transform = "translate(-50%, -50%)"; // Reset transform for non-attack movement
+        const player = document.getElementById("player");
+        const stepSize = 10;
+      
+        const playerRect = player.getBoundingClientRect();
+      
+        const currentLeft = playerRect.left;
+        const currentTop = playerRect.top;
+      
+        const newLeft = currentLeft + deltaX;
+        const newTop = currentTop + deltaY;
+      
+        // Set boundaries for left and right
+        const maxWidth = document.getElementById("game").offsetWidth;
+        const maxHeight = document.getElementById("game").offsetHeight;
+      
+        // Ensure the new position is within bounds
+        const boundedLeft = Math.max(
+          0,
+          Math.min(newLeft, maxWidth - player.clientWidth),
+        );
+        const boundedTop = Math.max(
+          0,
+          Math.min(newTop, maxHeight - player.clientHeight),
+        );
+      
+        player.style.left = `${boundedLeft}px`;
+        player.style.top = `${boundedTop}px`;
+      
+        if (!attacking) {
+          player.style.transform = "translate(-50%, -50%)"; // Reset transform for non-attack movement
+        }
       }
-    }
 
     function startAttack() {
       attacking = true;

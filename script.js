ca9252c6aaf013c8ae8b13eac87142013f14201b
Jcasanova1990 <jeremykcasanova@gmail.com>
2023-11-29 20:46:31 -0500

    'maps added'

--- script.js
+++ script.js
@@ -3,6 +3,59 @@ document.addEventListener("DOMContentLoaded", function () {
   let playerX = 300;
   let playerY = 250;
   let heldDirection = null;
+  let currentMapIndex = 0;
+
+  const maps = [
+      [
+          // Map 1
+          "#########",
+          "#       #",
+          "#       #",
+          "#       #",
+          "#       #",
+          "#########",
+      ],
+      [
+          // Map 2
+          "#########",
+          "#       #",
+          "#  ###  #",
+          "#       #",
+          "#       #",
+          "#########",
+      ],
+      [
+          // Map 3
+          "#########",
+          "#   #   #",
+          "#   #   #",
+          "#   #   #",
+          "#   #   #",
+          "#########",
+      ],
+  ];
+
+  function buildMap() {
+    const currentMap = maps[currentMapIndex];
+    const mapContainer = document.getElementById("game-container");
+
+    mapContainer.innerHTML = "";
+
+    for (let row = 0; row < currentMap.length; row++) {
+        for (let col = 0; col < currentMap[row].length; col++) {
+            const tile = document.createElement("div");
+            tile.className = "tile";
+            tile.style.left = col * 40 + "px";
+            tile.style.top = row * 40 + "px";
+
+            if (currentMap[row][col] === "#") {
+                tile.classList.add("obstacle");
+            }
+
+            mapContainer.appendChild(tile);
+        }
+    }
+}
 
   function updatePlayerPosition() {
     player.style.left = playerX + "px";
@@ -50,6 +103,8 @@ document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener("keydown", handleKeyPress);
   document.addEventListener("keyup", handleKeyRelease);
 
+  buildMap();
+  
   function gameLoop() {
     movePlayer();
     requestAnimationFrame(gameLoop);

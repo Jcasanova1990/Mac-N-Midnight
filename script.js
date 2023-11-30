4a0f2a4ccbfd850688c9a1ae7e4bda2e8548859a
Jcasanova1990 <jeremykcasanova@gmail.com>
2023-11-29 20:33:20 -0500

    'fully updated code'

--- script.js
+++ script.js
@@ -1,81 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const player = document.getElementById("player");
+  let playerX = 300;
+  let playerY = 250;
+  let heldDirection = null;
 
-    document.addEventListener("DOMContentLoaded", function () {
-    const player = document.getElementById("player");
-    console.log(player.style.left, player.style.top);
-    let attacking = false;
+  function updatePlayerPosition() {
+    player.style.left = playerX + "px";
+    player.style.top = playerY + "px";
+  }
 
-    document.addEventListener("keydown", function (event) {
-      handleKeyPress(event.key);
-    });
+  function handleKeyPress(e) {
+    switch (e.key) {
+      case "ArrowUp":
+      case "ArrowDown":
+      case "ArrowLeft":
+      case "ArrowRight":
+        heldDirection = e.key;
+        break;
+    }
+  }
 
-    document.addEventListener("keyup", function (event) {
-      if (event.key === " ") {
-        stopAttack();
-      }
-    });
+  function handleKeyRelease(e) {
+    if (e.key === heldDirection) {
+      heldDirection = null;
+    }
+  }
 
-    function handleKeyPress(key) {
-      const stepSize = 10;
+  function movePlayer() {
+    const speed = 5;
 
-      switch (key) {
-        case "ArrowUp":
-          movePlayer(0, -stepSize);
-          break;
-        case "ArrowDown":
-          movePlayer(0, stepSize);
-          break;
-        case "ArrowLeft":
-          movePlayer(-stepSize, 0);
-          break;
-        case "ArrowRight":
-          movePlayer(stepSize, 0);
-          break;
-        case " ":
-          startAttack();
-          break;
-      }
+    switch (heldDirection) {
+      case "ArrowUp":
+        playerY -= Speed;
+        break;
+      case "ArrowDown":
+        playerY += speed;
+        break;
+      case "ArrowLeft":
+        playerX -= speed;
+        break;
+      case "ArrowRight":
+        playerX += speed;
+        break;
     }
 
-    function movePlayer(deltaX, deltaY) {
-      const player = document.getElementById("player");
-      const stepSize = 10;
+    updatePlayerPosition();
+  }
 
-      const currentLeft = parseInt(player.style.left) || 0;
+  document.addEventListener("keydown", handleKeyPress);
+  document.addEventListener("keyup", handleKeyRelease);
 
-      const currentTop = parseInt(player.style.top) || 0;
-      console.log(player.style.left, player.style.top);
-      const newLeft = currentLeft + deltaX;
-      const newTop = currentTop + deltaY;
+  function gameLoop() {
+    movePlayer();
+    requestAnimationFrame(gameLoop);
+  }
 
-      // Set boundaries for left and right
-      const maxWidth = document.getElementById("game").offsetWidth;
-      const maxHeight = document.getElementById("game").offsetHeight;
-
-      // Ensure the new position is within bounds
-      const boundedLeft = Math.max(
-        0,
-        Math.min(newLeft, maxWidth - player.clientWidth),
-      );
-      const boundedTop = Math.max(
-        0,
-        Math.min(newTop, maxHeight - player.clientHeight),
-      );
-
-      player.style.left = `${boundedLeft}px`;
-      player.style.top = `${boundedTop}px`;
-
-      if (!attacking) {
-        player.style.transform = "translate(-50%, -50%)"; // Reset transform for non-attack movement
-      }
-    }
-
-    function startAttack() {
-      attacking = true;
-      player.style.transform = "translate(-50%, -50%) scale(1.5)"; // Example: Increase size for attack
-    }
-
-    function stopAttack() {
-      attacking = false;
-      player.style.transform = "translate(-50%, -50%)"; // Reset transform after attack
-    }
-  });
+  gameLoop();
+  updatePlayerPosition();
+});

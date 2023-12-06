79957e5d79b2ed3ac96558342789fcb50bc6b963
Jcasanova1990 <jeremykcasanova@gmail.com>
2023-12-05 19:49:05 -0500

    'player attack range fixed enemy/boss hits register and player attacks no longer act self inflict damage to player'

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const playerNameElement = document.getElementById("player-name");
     const playerHpElement = document.getElementById("player-hp");
     const player = document.getElementById("player");
-  
+
     playerNameElement.textContent = playerName;
-  
+
     let playerX = 300;
     let playerY = 250;
     let playerHealth = 15;
@@ -17,8 +17,8 @@ document.addEventListener("DOMContentLoaded", function () {
     let enemies = [];
     let bosses = [];
     const maxEnemyHits = 2;
-    const maxBossHits = 1;
-  
+    const maxBossHits = 10;
+
     const maps = {
         1: {
             width: 640,
@@ -35,106 +35,104 @@ document.addEventListener("DOMContentLoaded", function () {
             barrierX: 1,
         },
     };
-  
+
     const commonBoundaries = {
         width: 640,
         height: 360,
     };
-  
-    
-  
+
     function createBoss() {
-      if (currentMap === 3) {
-          const boss = document.createElement("div");
-          boss.classList.add("doodle", "boss");
-          boss.style.backgroundColor = "#800080";
-          document.getElementById("game-container").appendChild(boss);
-  
-          const maxX = maps[currentMap].width - 40;
-          const maxY = maps[currentMap].height - 40;
-          boss.style.left = Math.floor(Math.random() * maxX) + "px";
-          boss.style.top = Math.floor(Math.random() * maxY) + "px";
-  
-          boss.hits = 0; // Track the number of hits
-          boss.isHit = false; // Flag to track if the boss is hit
-  
-          bosses.push(boss);
-  
-          const attackInterval = setInterval(() => {
-              bosses.forEach((boss) => {
-                  if (currentMap === 3) {
-                      attackEnemy(boss);
-                  }
-              });
-          }, 1000);
-  
-          // Set up an interval to periodically check and remove the boss
-          const checkBossInterval = setInterval(() => {
-            if (boss.hits >= maxBossHits) {
-              clearInterval(attackInterval);
-              clearInterval(checkBossInterval);
-              boss.remove();
-              const index = bosses.indexOf(boss);
-              if (index !== -1) {
-                  bosses.splice(index, 1);
-                  console.log("Defeated boss:", boss);
-          
-                  // Additional actions upon defeating the boss
-                  // Trigger some event, display a message, etc.
-                  displayVictoryMessage();
-              }
-          }
-          }, 100); // Adjust the interval duration as needed
-      }
-  }
-  
-  function updateBosses() {
-    bosses.forEach((boss) => {
-        const bossX = parseInt(boss.style.left);
-        const bossY = parseInt(boss.style.top);
+        if (currentMap === 3) {
+            const boss = document.createElement("div");
+            boss.classList.add("doodle", "boss");
+            boss.style.backgroundColor = "#800080";
+            document.getElementById("game-container").appendChild(boss);
 
-        // Check for collision with the player
-        if (isPlayerCloseToEnemy(boss)) {
-            handleCollision(boss);
-        } else {
-            // Move towards the player
-            const angle = Math.atan2(playerY - bossY, playerX - bossX);
-            const speed = 2;
-            const deltaX = speed * Math.cos(angle);
-            const deltaY = speed * Math.sin(angle);
+            const maxX = maps[currentMap].width - 40;
+            const maxY = maps[currentMap].height - 40;
+            boss.style.left = Math.floor(Math.random() * maxX) + "px";
+            boss.style.top = Math.floor(Math.random() * maxY) + "px";
 
-            const newX = Math.max(0, Math.min(bossX + deltaX, maps[currentMap].width - 40));
-            const newY = Math.max(0, Math.min(bossY + deltaY, maps[currentMap].height - 40));
+            boss.hits = 0; // Track the number of hits
+            boss.isHit = false; // Flag to track if the boss is hit
 
-            boss.style.left = newX + "px";
-            boss.style.top = newY + "px";
+            bosses.push(boss);
 
-            // Reset the hit status for the next collision check
-            boss.isHit = false;
+            const attackInterval = setInterval(() => {
+                bosses.forEach((boss) => {
+                    if (currentMap === 3) {
+                        // Attack logic for boss (if needed)
+                    }
+                });
+            }, 1000);
+
+            // Set up an interval to periodically check and remove the boss
+            const checkBossInterval = setInterval(() => {
+                if (boss.hits >= maxBossHits) {
+                    clearInterval(attackInterval);
+                    clearInterval(checkBossInterval);
+                    boss.remove();
+                    const index = bosses.indexOf(boss);
+                    if (index !== -1) {
+                        bosses.splice(index, 1);
+                        console.log("Defeated boss:", boss);
+
+                        // Additional actions upon defeating the boss
+                        // Trigger some event, display a message, etc.
+                        displayVictoryMessage();
+                    }
+                }
+            }, 100); // Adjust the interval duration as needed
         }
-    });
-}
-  
+    }
+
+    function updateBosses() {
+        bosses.forEach((boss) => {
+            const bossX = parseInt(boss.style.left);
+            const bossY = parseInt(boss.style.top);
+
+            // Check for collision with the player
+            if (isPlayerCloseToEnemy(boss)) {
+                handleCollision(boss);
+            } else {
+                // Move towards the player
+                const angle = Math.atan2(playerY - bossY, playerX - bossX);
+                const speed = 2;
+                const deltaX = speed * Math.cos(angle);
+                const deltaY = speed * Math.sin(angle);
+
+                const newX = Math.max(0, Math.min(bossX + deltaX, maps[currentMap].width - 40));
+                const newY = Math.max(0, Math.min(bossY + deltaY, maps[currentMap].height - 40));
+
+                boss.style.left = newX + "px";
+                boss.style.top = newY + "px";
+
+                // Reset the hit status for the next collision check
+                boss.isHit = false;
+            }
+        });
+    }
+
     function createEnemy() {
         const enemy = document.createElement("div");
         enemy.classList.add("doodle", "enemy");
         enemy.hits = 0; // Track the number of hits
         enemy.isHit = false; // Flag to track if the enemy is hit
         document.getElementById("game-container").appendChild(enemy);
-  
+
         const maxX = maps[currentMap].width - 40;
         const maxY = maps[currentMap].height - 40;
         enemy.style.left = Math.floor(Math.random() * maxX) + "px";
         enemy.style.top = Math.floor(Math.random() * maxY) + "px";
-  
+
         enemies.push(enemy);
     }
-  
+
     function updateEnemies() {
         enemies.forEach((enemy) => {
             const enemyX = parseInt(enemy.style.left);
             const enemyY = parseInt(enemy.style.top);
-    
+
             // Check for collision with the player
             if (isPlayerCloseToEnemy(enemy)) {
                 handleCollision(enemy);
@@ -144,33 +142,34 @@ document.addEventListener("DOMContentLoaded", function () {
                 const speed = 2;
                 const deltaX = speed * Math.cos(angle);
                 const deltaY = speed * Math.sin(angle);
-    
+
                 const newX = Math.max(0, Math.min(enemyX + deltaX, maps[currentMap].width - 40));
                 const newY = Math.max(0, Math.min(enemyY + deltaY, maps[currentMap].height - 40));
-    
+
                 enemy.style.left = newX + "px";
                 enemy.style.top = newY + "px";
-    
+
                 // Reset the hit status for the next collision check
                 enemy.isHit = false;
             }
         });
     }
+
     function handleCollision(enemy) {
         if (!enemy.isHit) {
             // Only decrease player health if the enemy successfully collides
             playerHealth--;
-    
+
             // Check if player has run out of health
             if (playerHealth <= 0) {
                 // Game over logic (e.g., display a message, reset the game, etc.)
                 console.log("Game over!");
                 resetGame(); // Create a function to reset the game state
             }
-    
+
             // Mark the enemy as hit to prevent continuous damage
             enemy.isHit = true;
-    
+
             // Log the type of attack based on the enemy class
             if (enemy.classList.contains("boss")) {
                 console.log("Boss attacked!");
@@ -184,77 +183,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const victoryMessageElement = document.getElementById("victory-message");
         victoryMessageElement.textContent = "Congratulations! You defeated the boss! Thank you for playing the Pre-Alpha Demo. Please Refresh browser if you would like to play again!";
         victoryMessageElement.style.display = "block";
-    
+
         // resetting the game after a delay
         setTimeout(() => {
-            victoryMessageElement.style.display = "none"; 
+            victoryMessageElement.style.display = "none";
         }, 990000); // Adjust the delay as needed
     }
 
-
-    function attackEnemy(enemy) {
-        if (!enemy.isHit && isPlayerAttacking) {
-            enemy.style.backgroundColor = "#f00";
-            setTimeout(() => {
-                enemy.style.backgroundColor = "#ff0";
-            }, 1500);
-    
-            console.log("enemy Attacked!");
-    
-            enemy.hits = (enemy.hits || 0) + 1;
-            enemy.isHit = true; // Mark the enemy as hit
-    
-            const maxHits = enemy.classList.contains("boss") ? maxBossHits : maxEnemyHits;
-    
-            if (enemy.hits >= maxHits) {
-                if (isPlayerAttacking) {
-                    const index = enemy.classList.contains("boss") ? bosses.indexOf(enemy) : enemies.indexOf(enemy);
-                    if (index !== -1) {
-                        if (enemy.classList.contains("boss")) {
-                            bosses.splice(index, 1);
-                            console.log("Defeated boss:", enemy);
-    
-                            // Additional actions upon defeating the boss
-                            // Trigger some event, display a message, etc.
-                            displayVictoryMessage();
-                        } else {
-                            enemies.splice(index, 1);
-                            console.log("Defeated enemy:", enemy);
-                        }
-    
-                        enemy.remove();
-                    }
-                }
-            } else {
-                // Decrease player health with each attack
-                playerHealth--;
-    
-                // Check if player has run out of health
-                if (playerHealth <= 0) {
-                    // Game over logic (e.g., display a message, reset the game, etc.)
-                    console.log("Game over!");
-                    resetGame(); // Create a function to reset the game state
-                }
-            }
-        }
-    }
-    
-    
-    
-    
-    function clearEnemy() {
-        const enemiesToRemove = document.querySelectorAll(".doodle.enemy", ".doodle.boss");
-        enemiesToRemove.forEach((enemy) => {
-            enemy.remove();
-            const index = enemies.indexOf(enemy);
-            if (index !== -1) {
-                enemies.splice(index, 1);
-            }
-        });
-    }
-
-
-    // player controls/funcs
     function updatePlayerInfo() {
         // Update player HP in the UI
         playerHpElement.textContent = `HP: ${playerHealth}`;
@@ -263,13 +198,12 @@ document.addEventListener("DOMContentLoaded", function () {
     let lastAttackTime = 0;
     const attackCooldown = 1000; // 1000ms cooldown between attacks
     let isPlayerAttacking = false;
-  
+
     function isPlayerCloseToEnemy(enemy) {
         const playerDistance = Math.hypot(playerX - parseInt(enemy.style.left), playerY - parseInt(enemy.style.top));
-        return playerDistance < 80; // Change the value (80) to the desired attack range
+        return playerDistance < 50;
     }
-    
-    // Update the attack function to iterate through enemies and bosses and handle a single attack
+
     function attack() {
         const currentTime = Date.now();
         if (currentTime - lastAttackTime >= attackCooldown) {
@@ -286,28 +220,61 @@ document.addEventListener("DOMContentLoaded", function () {
     
             // Iterate through enemies and bosses and handle a single attack
             [...enemies, ...bosses].forEach((enemy) => {
-                if (isPlayerCloseToEnemy(enemy)) {
-                    attackEnemy(enemy);
+                const playerDistance = Math.hypot(playerX - parseInt(enemy.style.left) + 20, playerY - parseInt(enemy.style.top) + 20);
+                console.log(`Player distance to enemy: ${playerDistance}`);
+    
+                // Adjust the condition to check if the playerDistance is within the desired range
+                if (playerDistance < 120) { // Changes the value to the desired attack range
+                    enemy.style.backgroundColor = "#f00";
+                    setTimeout(() => {
+                        enemy.style.backgroundColor = "#ff0";
+                    }, 1500);
+    
+                    console.log("enemy Attacked!");
+    
+                    enemy.hits = (enemy.hits || 0) + 1;
+                    enemy.isHit = true; // Mark the enemy as hit
+    
+                    const maxHits = enemy.classList.contains("boss") ? maxBossHits : maxEnemyHits;
+    
+                    if (enemy.hits >= maxHits) {
+                        const index = enemy.classList.contains("boss") ? bosses.indexOf(enemy) : enemies.indexOf(enemy);
+                        if (index !== -1) {
+                            if (enemy.classList.contains("boss")) {
+                                bosses.splice(index, 1);
+                                console.log("Defeated boss:", enemy);
+    
+                                // Additional actions upon defeating the boss
+                                // Trigger some event, display a message, etc.
+                                displayVictoryMessage();
+                            } else {
+                                enemies.splice(index, 1);
+                                console.log("Defeated enemy:", enemy);
+                            }
+    
+                            enemy.remove();
+                        }
+                    }
                 }
             });
         }
     }
     
-  
+
     function block() {
         player.style.backgroundColor = "#0f0";
         setTimeout(() => {
             player.style.backgroundColor = "#00f";
         }, 300);
-  
+
         console.log("Player blocked!");
     }
-  
+
     function updatePlayerPosition() {
         player.style.left = playerX + "px";
         player.style.top = playerY + "px";
     }
-  
+
     function handleKeyPress(e) {
         switch (e.key) {
             case "ArrowUp":
@@ -324,25 +291,20 @@ document.addEventListener("DOMContentLoaded", function () {
                 break;
         }
     }
-  
+
     function handleKeyRelease(e) {
         if (e.key === heldDirection) {
             heldDirection = null;
         }
     }
-  
-    function isPlayerCloseToEnemy(enemy) {
-        const playerDistance = Math.hypot(playerX - parseInt(enemy.style.left), playerY - parseInt(enemy.style.top));
-        return playerDistance < 50;
-    }
-  
+
     function movePlayer() {
         const speed = 5;
-  
+
         switch (heldDirection) {
             case "ArrowUp":
                 playerY -= speed;
-  
+
                 if (playerY < 0 && currentMap === 1) {
                     currentMap = 2;
                     makeMapEnemies(currentMap);
@@ -350,75 +312,84 @@ document.addEventListener("DOMContentLoaded", function () {
                 } else if (playerY < 0 && currentMap === 2) {
                     currentMap = 3;
                     makeMapEnemies(currentMap);
-  
+
                     playerY = maps[currentMap].height - 40;
                 }
-  
+
                 if (currentMap === 3 && playerY < maps[3].barrierY) {
                     playerY = maps[3].barrierY;
                 }
                 break;
             case "ArrowDown":
                 playerY += speed;
-  
+
                 if (playerY > commonBoundaries.height - 40) {
                     playerY = commonBoundaries.height - 40;
                 }
                 break;
             case "ArrowLeft":
                 playerX -= speed;
-  
+
                 if (playerX < 0) {
                     playerX = 0;
                 }
                 break;
             case "ArrowRight":
                 playerX += speed;
-  
+
                 if (playerX > commonBoundaries.width - 40) {
                     playerX = commonBoundaries.width - 40;
                 }
                 break;
         }
-  
+
         updatePlayerPosition();
     }
-  
+
     function gameLoop() {
         movePlayer();
         updateEnemies();
         updateBosses();
         updatePlayerInfo();
         requestAnimationFrame(gameLoop);
-        
     }
-  
+
     function makeMapEnemies(currentMap) {
         clearEnemy();
-  
+
         if (currentMap === 1) {
             for (let i = 0; i < Math.min(enemyCount, 3); i++) {
                 createEnemy();
             }
         }
-  
+
         if (currentMap === 2) {
             for (let i = 0; i < Math.min(enemyCount, 5); i++) {
                 createEnemy();
             }
         }
-  
+
         if (currentMap === 3) {
             for (let i = 0; i < Math.min(bossCount, 1); i++)
-            createBoss();
+                createBoss();
         }
     }
-  
+
+    function clearEnemy() {
+        const enemiesToRemove = document.querySelectorAll(".doodle.enemy", ".doodle.boss");
+        enemiesToRemove.forEach((enemy) => {
+            enemy.remove();
+            const index = enemies.indexOf(enemy);
+            if (index !== -1) {
+                enemies.splice(index, 1);
+            }
+        });
+    }
+
     makeMapEnemies(1);
     gameLoop();
     updatePlayerPosition();
-  
+
     document.addEventListener("keydown", handleKeyPress);
     document.addEventListener("keyup", handleKeyRelease);
-  });
-
+});
